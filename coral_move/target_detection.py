c08f0c8ebfa089ac7e4b711dc0b54eedfbc0f77b
DrewEandi27 <157325726+DrewEandi27@users.noreply.github.com>
2024-03-02 12:41:41 -0800

    Update and rename auto_movement.py to target_detection.py

--- coral_move/target_detection.py
+++ coral_move/target_detection.py
@@ -10,32 +10,22 @@ import numpy as np
 
 class Auto(Node):
    def __init__(self):
-       super().__init__("auto_movement")
+       super().__init__("target_detection")
       
        self.logger = self.get_logger()
-       self.logger.info("Auto Movement Node Online")
+       self.logger.info("Target Detection Node Online")
       
        self.bridge = CvBridge()
-       self.logger.info("CVBridge created")
-       # self.image = None
       
        self.status = self.create_publisher(Bool, 'reached', 10)
-       self.logger.info("publisher 'reached' created")
        self.camera_subscriber = self.create_subscription(Image, "camera_feed", self.img_callback, 10)
-       self.logger.info("'camera_feed' subscriber created")
-       # self.procedure()
 
    def img_callback(self, msg):
-       # self.image = msg.data
        msg2 = Bool()
-       # img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-       # self.get_logger().info('Publishing: "%s"' % msg.data)
 
        imageFrame = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
        h, w, c = imageFrame.shape
        frameHeight = int(w)
-       # frameHeight = int(imageFrame.get(cv2.CAP_PROP_FRAME_HEIGHT))
-       # self.logger.info("imageFrame assigned")
 
        hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
         
@@ -66,10 +56,10 @@ class Auto(Node):
 
 def main():
    rclpy.init()
-   auto_movement = Auto()
-   rclpy.spin(auto_movement)
+   target_detection = Auto()
+   rclpy.spin(target_detection)
   
-   auto_movement.destroy_node()
+   target_detection.destroy_node()
    rclpy.shutdown()
 
 
