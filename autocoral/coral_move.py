3d42b1f14b847e505c3f0a8ba07e277ae0eb010b
mightylemongrass <157245866+mightylemongrass@users.noreply.github.com>
2024-04-13 10:15:25 -0700

    Update coral_move.py

--- autocoral/coral_move.py
+++ autocoral/coral_move.py
@@ -4,17 +4,28 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Bool
 from std_msgs.msg import Float32
 from rclpy.parameter import Parameter
+from geometry_msgs.msg import Vector3
+
+
+#dimensions of the camera frame: 1920 by 1080
 
 class Auto(Node):
     def __init__(self):
         super().__init__('Auto')
         self.cam_subscriber = self.create_subscription(Bool, 'reached', self.callback2, 10)
+        self.location_subscriber = self.create_subscription(Vector3, "coordinates", self.callback4, 10)
         self.sensor_subscriber = self.create_subscription(Float32, 'depth_sensor', self.callback, 10)
+        self.imu_subscriber = self.create_subscription(Vector3, "orientation_sensor", self.callback3, 10)
         self.vector = self.create_publisher(Twist, 'autovector', 10)
         self.logger = self.get_logger()
         self.box = False
         self.check = False
         self.declare_parameter('autonomous_task', self.boolean)
+        self.x = 0;
+        self.coordinatex = 0
+        self.coordinatey = 0
+        self.rangex = []
+        self.rangey = []
         #self.create_timer(0.1, self.testing)
 #    def testing(self):
 #        self.boolean = self.get_parameter('autonomous_task').value
@@ -25,26 +36,31 @@ class Auto(Node):
         self.boolean = self.get_parameter('autonomous_task').value
         if self.boolean == True:
             vector = Twist()
-            if msgs.data < 10 and self.check == False:
-                vector.linear.x = 0
-                vector.linear.y = 0
+            vector.linear.x = 0
+            vector.linear.x = 0
+            vector.linear.y = 0
+            vector.linear.z = 0
+            vector.angular.x = 0
+            vector.angular.z = 0
+            if msgs.data < 9 and self.check == False:
                 vector.linear.z = 0.5
-                vector.angular.x = 0
-                vector.angular.z = 0
             else:
                 self.check = True
                 if self.box == False:
                     vector.linear.x = 0.5
-                    vector.linear.y = 0
-                    vector.linear.z = 0
-                    vector.angular.x = 0
-                    vector.angular.z = 0
+                    vector.angular.x = 1-self.x
                 elif self.box == True and msgs.data > 11:
-                    vector.linear.x = 0
-                    vector.linear.y = 0
-                    vector.linear.z = -0.5
-                    vector.angular.x = 0
-                    vector.angular.z = 0
+                    if self.coordinatex < self.rangex[0]:
+                        self.linear.y = -0.3
+                    if self.coordinatex > self.rangex[1]:
+                        self.linear.y = 0.3
+                    if self.coordinatey < self.rangey[0]:
+                        self.linear.x = -0.3
+                    if self.coordinatey > self.rangey[1]:
+                        vector.linear.y = 0.3
+                    else:
+                        vector.linear.z = -0.5
+
                 else:
                     temp = Parameter(
                     'autonomous_task',
@@ -56,6 +72,11 @@ class Auto(Node):
     def callback2(self, msgs):
         self.box = msgs.data
         self.logger.info(self.box)
+    def callback3(self, msgs):
+        self.x = msgs.x;
+    def callback4(self, msgs):
+        self.coordinatex = msgs.x
+        self.coordinatey = msgs.y
         
 
 
